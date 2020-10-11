79f54d831f653f40fab703e020d51dfe2626f0d2
CYLEE <btstar_cy@korea.ac.kr>
2020-10-11 16:19:17 +0900

    Implemented logic

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const http = require('http');
+const axios = require('axios');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
+const server = http.createServer(async (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    try {
+        const url = `https://api.github.com/repos/nodejs/node`;
+        const { data } = await axios.get(url);
+        const stargazersCount = data['stargazers_count'];
+        const forksCount = data['forks_count'];
+        const openIssuesCount = data['open_issues_count'];
+        const message = `Stargazers: ${stargazersCount}\nForks: ${forksCount}\nOpen Issues: ${openIssuesCount}`;
+        
+        res.statusCode = 200;
+        res.end(message);
+    } catch (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end('Internal error issued.');
+    }
 });
 
 server.listen(port, hostname, () => {
